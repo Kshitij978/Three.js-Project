fa7bc51680dbd239dab13f6910654dfd189e4f2f
Kshitij Srivastava <42491256+Kshitij978@users.noreply.github.com>
2020-04-22 13:51:49 +0530

    Add files via upload

---
+++ Import 3D model/js/Model.js
@@ -0,0 +1,46 @@
+var scene, cam, renderer;
+            function init (){
+                //Scene
+                scene = new THREE.Scene();
+
+                //Camera
+                cam = new THREE.PerspectiveCamera(40, window.innerWidth/innerHeight, 1, 50000);
+                cam.rotation.y = 45/180 * Math.PI;
+                cam.position.set(-10,0,10);
+                cam.lookAt(new THREE.Vector3(0,0,0));
+
+                controls = new THREE.OrbitControls(cam);
+                controls.addEventListener('change', renderer);
+                
+                //Render
+                renderer = new THREE.WebGLRenderer();
+                renderer.setSize(window.innerWidth, window.innerHeight);
+                document.body.appendChild(renderer.domElement);
+
+                
+                var loader = new THREE.GLTFLoader();
+                loader.load('Experiment.glb', function(gltf){
+                    
+                    scene.add(gltf.scene);
+                    animate();
+                
+                });
+            }
+
+            function animate()
+            {
+                renderer.render(scene, cam);
+                requestAnimationFrame(animate);
+            }
+            init();
+
+            // Event Listeners
+            //resize the scene on window resize.
+            // -----------------------------------------------------------------------------
+            window.addEventListener( 'resize', onWindowResize, false );
+
+            function onWindowResize( event ) {
+                cam.aspect = window.innerWidth / window.innerHeight;
+                cam.updateProjectionMatrix();
+                renderer.setSize(window.innerWidth, window.innerHeight);
+            }

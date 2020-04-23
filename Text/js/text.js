25d58ab98b9ad16af857843bdd83ebb64dac0a69
Kshitij Srivastava <42491256+Kshitij978@users.noreply.github.com>
2020-04-23 23:02:06 +0530

    3D text on web.
    Creating 3D text on web example using FontLoader.

---
+++ Text/js/text.js
@@ -0,0 +1,58 @@
+var cam, renderer;
+//scene.
+const scene = new THREE.Scene();
+            {
+                const color = 0xFFFFFF;
+                const intensity = 1;
+                const light = new THREE.DirectionalLight(color, intensity);
+                light.position.set(-10, 0, 40);
+                scene.add(light);
+            
+            }
+//camera.
+cam = new THREE.PerspectiveCamera(45, window.innerWidth/innerHeight, 0.1, 1000)
+cam.position.z = 800;
+cam.lookAt(new THREE.Vector3(350, 0, -350));
+            
+//camera control.
+controls = new THREE.OrbitControls(cam);
+controls.addEventListener('change', renderer);
+
+//render.
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+            
+//load font.
+var loader = new THREE.FontLoader();
+loader.load( 'fonts/Cinzel_Regular.json', function ( font ) {
+        //text geometry.
+        var textGeometry = new THREE.TextGeometry("KSHITIJ", {
+            font: font,
+            size: 80,
+		    height: 5,
+            style: "normal",
+		    curveSegments: 12,
+		    bevelEnabled: true,
+		    bevelThickness: 10,
+		    bevelSize: 8,
+		    bevelOffset: 0,
+            bevelSegments: 5
+            });
+        //text material.
+        var textMat = new THREE.MeshPhongMaterial({
+            specular: 0xffffff,
+            color: 0x907070,
+            shininess: 100,
+            metal: true
+        });
+        //text mesh.
+        var textMesh = new THREE.Mesh(textGeometry, textMat);
+            scene.add(textMesh);
+            animate();
+});
+
+function animate(){
+renderer.render(scene, cam);
+requestAnimationFrame(animate);
+}
